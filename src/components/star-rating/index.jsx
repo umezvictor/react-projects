c5d63b1a34389a6f73a62d88f1c916573e0a88e2
umezvictor <victor.umezuruike@toyingroup.com>
2024-05-31 09:26:31 +0100

    added star rating

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
+import "./style.css";
 
 export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   function handleClick(currentIndex) {
-    console.log(currentIndex);
+    setRating(currentIndex);
   }
 
   function handleMouseEnter(currentIndex) {
-    console.log(currentIndex);
+    setHover(currentIndex);
   }
 
-  function handleMouseLeave(currentIndex) {
-    console.log(currentIndex);
+  function handleMouseLeave() {
+    setHover(rating);
   }
 
   return (
     <div className="star-rating">
       {[...Array(noOfStars)].map((_, index) => {
-        index += 1;
+        index += 1; //increase index by 1, since it starts from 0
         return (
           <FaStar
             key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
             onClick={() => handleClick(index)}
             onMouseMove={() => handleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave(index)}
