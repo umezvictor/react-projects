57fc1e6d57470b5866f83bc44467542268d9e7b8
umezvictor <victor.umezuruike@toyingroup.com>
2024-05-31 15:38:35 +0100

    created an image slider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Accordion from "./components/accordion";
+import ImageSlider from "./components/image-slider";
 import RandomColour from "./components/random-colour";
 import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
-      {/* <Accordion /> */}
-
-      {/* <RandomColour /> */}
-      <StarRating />
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={"10"}
+        page={"1"}
+      />
     </div>
   );
 }
